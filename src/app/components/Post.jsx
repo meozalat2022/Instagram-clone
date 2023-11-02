0690e4e2fd1d2f08121b529ae3b23f9c1b8a6fb0
meozalat2022 <meozalat2002@hotmail.com>
2023-11-02 23:16:03 +0200

    posts

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -1,9 +1,44 @@
+import Image from "next/image";
 import React from "react";
 
-const Post = ({ key, id, username, img, caption }) => {
+const Post = ({ key, id, userImage, username, img, caption }) => {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* post header */}
+
+      <div className="flex items-center">
+        <Image
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          width={75}
+          height={75}
+          src={userImage}
+          alt={username}
+        ></Image>
+        <p className="font-bold flex-1">{username}</p>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+          />
+        </svg>
+      </div>
+      {/* post image */}
+      <Image
+        className="object-cover w-full"
+        width={350}
+        height={350}
+        src={img}
+        alt={username}
+      ></Image>
     </div>
   );
 };
--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -9,12 +9,14 @@ const Posts = () => {
       userImag:
         "https://images.unsplash.com/photo-1682687218904-de46ed992b58?auto=format&fit=crop&q=80&w=1843&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       caption: "beautiful view",
+      userImage: "https://i.pravatar.cc/150?img=2",
     },
     {
       id: "2",
       username: "Ahmed",
       userImag:
         "https://images.unsplash.com/photo-1698402532181-78b834538a86?auto=format&fit=crop&q=80&w=1954&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      userImage: "https://i.pravatar.cc/150?img=5",
       caption: "Nice Picture",
     },
     {
@@ -22,6 +24,7 @@ const Posts = () => {
       username: "Hassan",
       userImag:
         "https://images.unsplash.com/photo-1698778874232-6a0a58d84055?auto=format&fit=crop&q=80&w=2003&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      userImage: "https://i.pravatar.cc/150?img=7",
       caption: "Nice Image",
     },
     {
@@ -29,6 +32,7 @@ const Posts = () => {
       username: "Youssef",
       userImag:
         "https://images.unsplash.com/photo-1698785235568-90fe4cf251d2?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      userImage: "https://i.pravatar.cc/150?img=15",
       caption: "Nice view",
     },
   ];
@@ -40,6 +44,7 @@ const Posts = () => {
             key={post.id}
             id={post.id}
             username={post.username}
+            userImage={post.userImage}
             img={post.userImag}
             caption={post.caption}
           />
