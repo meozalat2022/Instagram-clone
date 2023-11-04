bdec7dbacce4568dd9cf81e3aeb3814e0d9ab241
meozalat2022 <meozalat2002@hotmail.com>
2023-11-04 22:35:49 +0200

    post bottom component

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -6,9 +6,9 @@ const Post = ({ key, id, userImage, username, img, caption }) => {
     <div className="bg-white my-7 border rounded-md">
       {/* post header */}
 
-      <div className="flex items-center">
+      <div className="flex items-center ">
         <Image
-          className="h-12 rounded-full object-cover border p-1 mr-3"
+          className="h-[75px] w-[75px] rounded-full object-cover border p-1 mr-3"
           width={75}
           height={75}
           src={userImage}
@@ -32,13 +32,55 @@ const Post = ({ key, id, userImage, username, img, caption }) => {
         </svg>
       </div>
       {/* post image */}
-      <Image
-        className="object-cover w-full"
-        width={350}
-        height={350}
-        src={img}
-        alt={username}
-      ></Image>
+      <img className="object-cover w-full" src={img} alt={userImage} />
+      {/* post button */}
+
+      <div className="flex justify-between px-4 pt-4 mb-1">
+        <div className="flex space-x-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7 hover:scale-125 transition-transform duration-200 ease-out cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7 hover:scale-125 transition-transform duration-200 ease-out cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+            />
+          </svg>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-7 h-7 hover:scale-125 transition-transform duration-200 ease-out cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
