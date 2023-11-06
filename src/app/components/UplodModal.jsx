6d8b23b8b84096bc7034fe33ae27392fd5d8844e
meozalat2022 <meozalat2002@hotmail.com>
2023-11-06 23:35:09 +0200

    finish modal

--- src/app/components/UplodModal.jsx
+++ src/app/components/UplodModal.jsx
@@ -1,20 +1,82 @@
 "use client";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Modal from "react-modal";
 import { modalState } from "../../../atom/modalAtomState";
 import { useRecoilState } from "recoil";
 const UplodModal = () => {
+  const filePictureRef = useRef(null);
   const [open, setOpen] = useRecoilState(modalState);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const addImageToPost = (event) => {
+    const reader = new FileReader();
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
   return (
     <div>
       {open && (
         <Modal
-          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          className="p-6 max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
           isOpen={open}
-          onRequestClose={() => setOpen(false)}
+          onRequestClose={() => {
+            setOpen(false);
+            setSelectedFile(null);
+          }}
         >
           <div className=" flex flex-col justify-center items-center h-full">
-            <h1>Modal</h1>
+            {selectedFile ? (
+              <img
+                onClick={() => setSelectedFile(null)}
+                className="w-full max-h-[250px] object-cover cursor-pointer"
+                src={selectedFile}
+                alt="useruploadedfile"
+              />
+            ) : (
+              <svg
+                onClick={() => filePictureRef.current.click()}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-14 h-14  bg-red-200 p-2 rounded-full border-2 text-red-500 cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"
+                />
+              </svg>
+            )}
+            <input
+              onChange={addImageToPost}
+              type="file"
+              hidden
+              ref={filePictureRef}
+            />
+            <input
+              type="text"
+              maxLength="150"
+              placeholder="Please enter your caption"
+              className="m-4 border-none text-center w-full focus-ring-0"
+            />
+            <button
+              disabled
+              className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+            >
+              Upload Post
+            </button>
           </div>
         </Modal>
       )}
